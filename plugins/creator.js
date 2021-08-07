bf4644598193993be48dce83ccb806371b5fa8b0
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-08-07 10:43:44 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
     
 
-    r_text[1] = "*╔═════😎POOCHA_SER😎═════╗*\n           \n*⚜═Sophia═⚜*\n\n*owner ALINSHAN - http://Wa.me/+919383491460*\n*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: ALINSHAN*"
+    r_text[1] = "*╔═════😎POOCHA_SER😎═════╗*\n           \n*⚜═POOCHA_SER═⚜*\n\n*owner ALINSHAN - http://Wa.me/+919383491460*\n*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: ALINSHAN*"
 
     
 
@@ -56,7 +56,7 @@ if (Config.WORKTYPE == 'private') {
 
     
 
-    r_text[1] = "*╔═════😎POOCHA_SER😎═════╗*\n           \n*⚜═POOCHA_SER═⚜*\n\n*owner ALINSHAN - http://Wa.me/+91938391460*\n*       *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: ALINSHAN*"
+    r_text[1] = "*╔═════😎POOCHA_SER😎═════╗*\n           \n*⚜═POOCHA_SER═⚜*\n\n*owner ALINSHAN - http://Wa.me/+919383491460*\n*       *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: ALINSHAN*"
 
     
 
